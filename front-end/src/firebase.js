ab426fd501e824b7355df8c83036ff6530774b97
Lakith Rathnayake <lakithpathmila@gmail.com>
2023-12-18 01:10:31 +0530

    Add firebase into the app

---
+++ front-end/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCbBnkT7QABS-GftWtbuEGpweGAGIIEqf0",
+  authDomain: "chatterbox-chat-app-16b1d.firebaseapp.com",
+  projectId: "chatterbox-chat-app-16b1d",
+  storageBucket: "chatterbox-chat-app-16b1d.appspot.com",
+  messagingSenderId: "159958590146",
+  appId: "1:159958590146:web:35006145966bfd7134515b",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
